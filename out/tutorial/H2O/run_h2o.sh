e211a3647306606760817831e6f2fc1085058c2d
Ryoya Hori <k022716@ohtaka1.issp.u-tokyo.ac.jp>
2024-04-15 15:58:28 +0900

    2024 Ohtaka out

--- out/tutorial/H2O/run_h2o.sh
+++ out/tutorial/H2O/run_h2o.sh
@@ -1,28 +1,27 @@
-#!/bin/bash
-#PBS -q DBG
-#PBS --group=k0227
-#PBS -m b
-#PBS -T intmpi
-#PBS -l cpunum_job=64
-#PBS -v OMP_NUM_THREADS=2
-#PBS -l elapstim_req=00:10:00
-#PBS -e ERR
-#PBS -o OUT
+#!/bin/sh
+#SBATCH -J h2o
+#SBATCH -p F4cpu
+#SBATCH -N 2
+#SBATCH -n 128
+#SBATCH -c 2
+#SBATCH -o LOG
+#SBATCH -e err
 
+#
+echo started at 'date'
+#
 module load oneapi_compiler/2023.0.0
 module load oneapi_mkl/2023.0.0
 module load oneapi_mpi/2023.0.0
-
+#
 INPUT_FILE='relax.in'
 OUTPUT_FILE='relax.out'
 #
 PW_DIR=${HOME}/QE/src/qe-7.2/bin
-#
-MPI_COMMAND="mpirun ${NQSV_MPIOPTS}"
 PW_COMMAND=pw.x
 PW=$PW_DIR/$PW_COMMAND
 #
-cd ${PBS_O_WORKDIR}
+srun ${PW} < ${INPUT_FILE} > ${OUTPUT_FILE}
 #
-${MPI_COMMAND} ${PW} < ${INPUT_FILE} > ${OUTPUT_FILE}
-
+ls -lrt
+echo finifhed at 'date'
