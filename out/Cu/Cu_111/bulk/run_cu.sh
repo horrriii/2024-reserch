74438b3ea1525d06398624ef1608cb00d743f545
Ryoya Hori <k022716@ohtaka1.issp.u-tokyo.ac.jp>
2024-05-09 16:26:04 +0900

    05/09 backup

--- out/Cu/Cu_111/bulk/run_cu.sh
+++ out/Cu/Cu_111/bulk/run_cu.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #SBATCH -J Cu_fcc
 #SBATCH -p F4cpu
-#SBATCH -N 2
+#SBATCH -N 4
 #SBATCH -n 128
-#SBATCH -c 2
+#SBATCH -c 4
 #SBATCH -o LOG
 #SBATCH -e ERR
 
@@ -17,4 +17,4 @@ module load oneapi_mpi/2023.0.0
 export ASE_ESPRESSO_COMMAND="mpirun ${HOME}/QE/src/qe-7.2/bin/pw.x -in PREFIX.pwi > PREFIX.pwo"
 export ESPRESSO_PSEUDO="/home/k0227/k022716/QE/pseudo"
  
-python3 ase-qe.py
+python3 bulk_run.py
