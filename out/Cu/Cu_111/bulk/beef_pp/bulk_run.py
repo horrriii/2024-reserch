d7b9382b193cb2cfd8e5d73372278c68d4d88ed8
horrriii <k022716@ohtaka1.issp.u-tokyo.ac.jp>
2024-06-07 10:18:44 +0900

    06/07 backup

--- out/Cu/Cu_111/bulk/beef_pp/bulk_run.py
+++ out/Cu/Cu_111/bulk/beef_pp/bulk_run.py
@@ -17,7 +17,7 @@ n_processors=76
 
 bulk_structure=bulk('Cu', 'fcc', a , cubic=True)
 
-for i in [950,1050,1200]:
+for i in [1200]:
     ecutwfc=i*(eV/Ry)
     kpt_mesh=[24,24,24]
 
--- out/Cu/Cu_111/slab/new_pbe/slab_run.py
+++ out/Cu/Cu_111/slab/new_pbe/slab_run.py
@@ -14,7 +14,7 @@ from ase.constraints import FixAtoms
 
 n_processors=76
 
-for i in [750,850,950,1050,1200]:
+for i in [850,950,1050,1200]:
     bulk=read('Cu-'+str(i)+'-3.61.pwo', index=-1)
 
     supercell_number=3
