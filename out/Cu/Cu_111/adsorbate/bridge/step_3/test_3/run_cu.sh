b41b9f1e1a4bbc7c4d9edae140489eebd7784c1f
horrriii <k022716@ohtaka1.issp.u-tokyo.ac.jp>
2024-07-04 09:46:58 +0900

    07/04 backup

---
+++ out/Cu/Cu_111/adsorbate/bridge/step_3/test_3/run_cu.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#SBATCH -J Cu_111
+#SBATCH -p F16cpu
+#SBATCH -N 6
+
+export FI_PROVIDER=psm3
+export OMP_NUM_THREADS=2
+ulimit -s unlimited
+#
+module purge
+module load oneapi_compiler/2023.0.0
+module load oneapi_mkl/2023.0.0
+module load oneapi_mpi/2023.0.0
+#
+#export ASE_ESPRESSO_COMMAND="srun ${HOME}/QE/src/qe-7.2/bin/pw.x -nk 3 -nb 32 -ntg 4 -nd 1 -in PREFIX.pwi > PREFIX.pwo"
+export ASE_ESPRESSO_COMMAND="srun --exclusive --mem-per-cpu=1840 -n 768 -c 1 -N 6 ${HOME}/QE/src/qe-7.2/bin/pw.x -nk 3 -nb 32 -ntg 8 -nd 1 -in PREFIX.pwi | tee PREFIX.pwo"
+export ESPRESSO_PSEUDO="/home/k0227/k022716/QE/pseudo"
+
+echo "========= Job started  at `date` =========="
+
+python3 slab_ads.py | tee python.log
+
+echo "========= Job finished at `date` =========="
--- out/Cu/Cu_111/adsorbate/ontop/beef_pp/run_cu.sh
+++ out/Cu/Cu_111/adsorbate/ontop/beef_pp/run_cu.sh
@@ -1,23 +1,21 @@
 #!/bin/sh
-#SBATCH --partition=F16cpu
-#SBATCH --nodes=16
-#SBATCH --ntasks-per-node=128
-#SBATCH --time=24:00:00
-#SBATCH --job-name=Cu_ads
+#SBATCH -J Cu_111
+#SBATCH -p i8cpu
+#SBATCH -N 6
 
 export FI_PROVIDER=psm3
+export OMP_NUM_THREADS=2
 ulimit -s unlimited
+#
+echo started at 'date'
 #
 module purge
 module load oneapi_compiler/2023.0.0
 module load oneapi_mkl/2023.0.0
 module load oneapi_mpi/2023.0.0
 #
-export ASE_ESPRESSO_COMMAND="mpirun ${HOME}/QE/src/qe-7.2/bin/pw.x -in PREFIX.pwi > PREFIX.pwo"
+#export ASE_ESPRESSO_COMMAND="srun ${HOME}/QE/src/qe-7.2/bin/pw.x -nk 3 -nb 32 -ntg 4 -nd 1 -in PREFIX.pwi > PREFIX.pwo"
+export ASE_ESPRESSO_COMMAND="srun --exclusive --mem-per-cpu=1840 -n 384 -c 2 -N 6 ${HOME}/QE/src/qe-7.2/bin/pw.x -nk 2 -nb 48 -ntg 4 -nd 1 -in PREFIX.pwi | tee PREFIX.pwo"
 export ESPRESSO_PSEUDO="/home/k0227/k022716/QE/pseudo"
 
-echo "========= Job started  at `date` =========="
-
-python3 slab_ads.py
-
-echo "========= Job finished at `date` =========="
+python3 slab_ads.py | tee python.log
